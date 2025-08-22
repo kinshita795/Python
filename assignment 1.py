857a0135c249d780b847be329e27552c93db88a5
kinshita795 <chaturvedikinshita@gmail.com>
2025-08-22 11:58:57 +0530

    Add files via upload
    this is assignment 1 and it contains some very basic arithmetic and string operations

---
+++ assignment 1.py
@@ -0,0 +1,13 @@
+#TASK 1 (basic operations)
+
+a = int(input("enter number 1:"))
+b = int(input("enter number 2:"))
+print("addition:", a+b )
+print("substraction:", a-b )
+print("multiplication:", a*b )
+print("division:", a/b )
+
+#TASK 2 (greetings)
+a= input("enter your first name:")
+b= input("enter your second name:")
+print("hello,", a+" "+b,"! welcome to the python program")
